b388024d2d23d18270882ede7225b010393fed9e
RobotKava345 <elonmusk9933@gmail.com>
2025-02-08 17:48:00 +0200

    new_changes

--- Maze-main/maze.py
+++ Maze-main/maze.py
@@ -1,5 +1,7 @@
 #створи гру "Лабіринт"!
 from pygame import *
+import random 
+init()
 
 FPS = 60
 TILE_SIZE = 40
@@ -39,7 +41,7 @@ class Player(BaseSprite):
         super().__init__(image, x, y, width, height)
         self.right_image = self.image
         self.left_image = transform.flip(self.image,True, False)
-        
+        self.damage_timer = time.get_ticks()       
         self.speed = 5
         self.hp = 100
         self.coins_counter = 0
@@ -67,13 +69,44 @@ class Player(BaseSprite):
         if len(coll_list)>0:
             self.rect.x, self.rect.y = old_pos
 
+        coll_list = sprite.spritecollide(self, enemys, False, sprite.collide_mask)
+        if len(coll_list)>0:
+            now = time.get_ticks()
+            if now - self.damage_timer > 1500:
+                self.damage_timer = time.get_ticks()
+                self.hp -=10
+                print(self.hp)
+            
+            self.rect.x, self.rect.y = old_pos
+            
+
+
 class Enemy(BaseSprite):
     def __init__(self, image, x, y, width, height):
         super().__init__(image, x, y, width, height)
         self.right_image = self.image
         self.left_image = transform.flip(self.image,True, False)
         self.speed = 2
-
+        self.dir_list = ['left', 'right', 'up', 'down']
+        self.dir = random.choice(self.dir_list)
+    def update(self):
+        old_pos = self.rect.x, self.rect.y
+        if self.dir == 'left':
+            self.rect.x -=self.speed
+            self.image = self.left_image
+        elif self.dir == 'right':
+           self.rect.x +=self.speed
+           self.image = self.right_image
+        elif self.dir == 'up':
+            self.rect.y -=self.speed
+        elif self.dir == 'down':
+            self.rect.y +=self.speed
+        
+        coll_list = sprite.spritecollide(self, walls, False, sprite.collide_mask)
+        if len(coll_list)>0:
+            self.rect.x, self.rect.y = old_pos
+            self.dir = random.choice(self.dir_list)
+        
 
 player1 = Player(player_img,200,300, TILE_SIZE-5, TILE_SIZE-5)
 
@@ -93,7 +126,7 @@ with open("map.txt", "r") as file:
                 player1.rect.y = y
 
             if symbol=='E':
-                enemys.add(Enemy(enemy_img, x, y, TILE_SIZE, TILE_SIZE))
+                enemys.add(Enemy(enemy_img, x, y, TILE_SIZE-5, TILE_SIZE-5))
 
             x+=TILE_SIZE
         x = 0    
@@ -103,15 +136,18 @@ with open("map.txt", "r") as file:
 
 
 run = True
+finish = False
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
     
+    if player1.hp<=0:
+        finish = True
+    if not finish:
+        player1.update()
 
-
-    player1.update()
-
+        enemys.update()
 
     window.blit(bg, (0,0))
     
